3e78e1f9d94e255279161e61f890da1585590e9a
voelkldennis <voelkldennis@googlemail.com>
2022-10-07 11:03:57 +0200

    Add Product class

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,5 +1,12 @@
+import Product.*;
 public class ShopService {
     public static void main(String[] args) {
         System.out.println("Willkommen im unserem Shopservice");
+        Product newProduct = new Product();
+        newProduct.setProductID(1);
+        newProduct.setProductName("Shampoo");
+        //System.out.println(newProduct.getProductID());
+        //System.out.println(newProduct.getProductName());
+        System.out.println(newProduct);
     }
 }
