0839e5cf0d2fe7342b3c2b43e6270c03ba2db975
voelkldennis <voelkldennis@googlemail.com>
2022-10-07 11:57:25 +0200

    Add List, Array List to Main

---
+++ src/main/java/Product/ProductRepo.java
@@ -0,0 +1,10 @@
+package Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ProductRepo{
+    //HashMap<Integer, Product> products;    //Mein 1. Produkt: Product{productID=1, productName='Shampoo'}
+    //List<Product> productList;
+}
